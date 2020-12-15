62732bad019c474c206ea11296bd35372b50fdb9
BisiDavi <oludavidconnect@gmail.com>
2020-12-15 01:20:38 +0100

    routes added

--- server/src/routes/posts.ts
+++ server/src/routes/posts.ts
@@ -1,2 +1,21 @@
-import express from 'express';
+import express from "express";
 
+import {
+  getPosts,
+  getPost,
+  createPost,
+  updatePost,
+  likePost,
+  deletePost
+} from "../imports";
+
+const router = express.Router();
+
+router.get("/", getPosts);
+router.get("/:id", getPost);
+router.get("/", createPost);
+
+router.patch("/:id", updatePost);
+router.patch("/:id/likePost", likePost);
+
+router.delete("/:id", deletePost);
